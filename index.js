ef8cd67aaef085ad060ef5147ca6d706c2e65302
MykhaelaD <114114621+MykhaelaD@users.noreply.github.com>
2024-04-07 23:02:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyABxSUHqekjFDhnyFGFKQd3weF-YaKw4ls",
-  authDomain: "seniorproject-7e271.firebaseapp.com",
-  databaseURL: "https://seniorproject-7e271-default-rtdb.firebaseio.com",
-  projectId: "seniorproject-7e271",
-  storageBucket: "seniorproject-7e271.appspot.com",
-  messagingSenderId: "449257268811",
-  appId: "1:449257268811:web:5c7b798acb6aff00758fce",
-  measurementId: "G-TXKSFRLVBJ"
-};
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
 const auth = getAuth();
+const database = getDatabase();
 
 document.addEventListener("DOMContentLoaded", function() {
-    const signUp = document.getElementById('signUp');
-    signUp.addEventListener('click', (e)=> { //check if this should be register link!!
-        var username = document.getElementById('username').value;
-        var email = document.getElementById('email1').value;
-        var password = document.getElementById('password1').value;
+    const signUpButton = document.getElementById('signUp');
 
+    signUpButton.addEventListener('click', (e) => {
+        e.preventDefault();
 
+        // Get user inputs
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Create user with email and password
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                // Signed up 
                 const user = userCredential.user;
-                console.log("Hello1")
-                set(ref(database, 'users/' + user.uid),{ //uid is the unique ID
-                    username: username, //saving the username to the database
-                    email: email //saving the email to the database
-                    //never save your password to the database
-            })
-                console.log("Hello2")
-                alert('user created!');
-                // ...
+
+                // Save user data to the database
+                saveUserData(user.uid, username, email);
+                
+                alert('User created successfully!');
+
+                // Redirect or perform other actions after successful sign up
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
-
                 alert(errorMessage);
-                // ..
             });
     });
-
 });
+
+function saveUserData(userId, username, email) {
+    // Define the path to the user data in the database
+    const userRef = ref(database, 'users/' + userId);
+
+    // Set user data
+    set(userRef, {
+        username: username,
+        email: email
+    });
+}
